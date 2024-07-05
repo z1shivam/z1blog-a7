a88a6213f8b21ba75e12354452ca4e5888c6e83c
z1shivam <shivamk.services@gmail.com>
2024-07-06 01:44:37 +0530

    implimented image store feature

--- src/app/(auth)/write/page.tsx
+++ src/app/(auth)/write/page.tsx
@@ -1,13 +1,16 @@
+import ImageStoreServerWrapper from "@/components/write/ISserverWrapper";
 import WriteFormServerWrapper from "@/components/write/WFserverWrapper";
 
 export default function WritePage() {
   return (
-    <main className="bg-gray-50">
-      <section className="mx-auto max-w-7xl p-4 space-x-4">
+    <main className="bg-gray-50 ">
+      <section className="mx-auto flex max-w-7xl space-x-6 p-4">
         <div className="w-3/5">
           <WriteFormServerWrapper />
         </div>
-        <aside className="w-2/5"></aside>
+        <aside className="h-fit w-2/5 rounded-md border-2 border-gray-300 bg-white pb-4">
+          <ImageStoreServerWrapper />
+        </aside>
       </section>
     </main>
   );
--- src/server/actions/imageUpload.ts
+++ src/server/actions/imageUpload.ts
@@ -2,7 +2,14 @@
 
 import cloudinary from "@/cloudinary";
 
-export async function uploadImage(formdata: FormData): Promise<{ url: string; success: string; error: string }> {
+export async function uploadImage(formdata: FormData): Promise<{
+  url: string;
+  success: string;
+  error: string;
+  width?: number;
+  height?: number;
+  size?: number;
+}> {
   const file = formdata.get("imageToUpload") as File;
   if (!file) {
     return { url: "", success: "", error: "No file provided" };
@@ -12,22 +19,39 @@ export async function uploadImage(formdata: FormData): Promise<{ url: string; su
   const buffer = new Uint8Array(arrayBuffer);
 
   try {
-    const result = await new Promise<{ url: string; success: string; error: string }>((resolve, reject) => {
-      cloudinary.uploader.upload_stream({}, (err, res) => {
-        if (err || !res) {
-          reject({ url: "", success: "", error: "Upload failed, try again" });
-        } else {
-          resolve({
-            url: res.url,
-            success: "Image uploaded successfully",
-            error: "",
-          });
-        }
-      }).end(buffer);
+    const result = await new Promise<{
+      url: string;
+      success: string;
+      error: string;
+      width?: number;
+      height?: number;
+      size?: number;
+    }>((resolve, reject) => {
+      cloudinary.uploader
+        .upload_stream({}, (err, res) => {
+          console.log(res);
+          if (err || !res) {
+            reject({ url: "", success: "", error: "Upload failed, try again" });
+          } else {
+            resolve({
+              url: res.url,
+              success: "Image uploaded successfully",
+              error: "",
+              width: res.width,
+              height: res.height,
+              size: res.bytes,
+            });
+          }
+        })
+        .end(buffer);
     });
 
     return result;
   } catch (error: any) {
-    return { url: "", success: "", error: error.error || "An unexpected error occurred" };
+    return {
+      url: "",
+      success: "",
+      error: error.error || "An unexpected error occurred",
+    };
   }
 }
